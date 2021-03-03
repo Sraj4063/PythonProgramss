1d57ce38f2136c197b4f4ee3cc8f7d2ca03b69b4
Sraj4063 <Sraja4063@gmail.com>
2021-03-03 16:30:07 +0530

    commit

---
+++ AccessingMembersOfObjectsOfCircle.py
@@ -0,0 +1,26 @@
+from Circle import Circle
+
+c =Circle(1)
+print(c.radius)
+print(c.getPerimeter())
+print(c.getArea())
+print("Area is ", Circle(5).getArea())
+
+class A:
+    i: int
+
+    def __init__(self, i = 1) -> object:
+        self.i = i
+def main():
+    a = A()
+    print(a.i)
+print("B")
+class B:
+    def A(self):
+        radius = 3
+print("C")
+class C:
+    def __init__(self):
+        radius = 3
+    def setRadius(radius):
+        self.radius = radius
